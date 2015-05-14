70d1adc4921a5b24d09482cafffe2102ee2a82ac
Philippe MARTIN <contact@elol.fr>
2015-05-14 20:40:12 +0200

    Add pgajax.php script# Please enter the commit message for your changes. Lines starting

---
+++ pgajax.php
@@ -0,0 +1,56 @@
+<?php
+require 'pgprocedures.class.php';
+require_once ('dbconfig.inc.php');
+$base = new PgProcedures ($pghost, $pguser, $pgpass, $pgbase);
+
+$cmd = $_SERVER['PHP_SELF'];
+$cmd = basename ($cmd, '.php');
+
+if (strpos($cmd, '@')) {
+  list($schema, $function) = explode('@', $cmd, 2);
+} else {
+  $schema = 'public';
+  $function = $cmd;
+}
+
+$debug = false;
+//echo $schema.' - '.$function."\n";
+$ret = $base->get_arguments ($schema, $function);
+$all = false;
+if (count ($ret)) {
+  foreach ($ret as $r) {
+    $all = true;
+    foreach ($r['argnames'] as $argname) {
+      if (!isset ($_REQUEST[$argname])) {
+	if ($debug) {
+	  echo "$argname not found\n" ;
+	  print_r ($_REQUEST);
+	}
+	$all = false;
+	break;
+      }
+    }
+    if ($all) {
+      break;
+    }    
+  }
+}
+
+// Continue, $r contains argnames and argtypes
+
+$args = array ();
+if ($all) {
+  foreach ($r['argnames'] as $argname) {
+    $args[] = get_magic_quotes_gpc() ? stripslashes($_REQUEST[$argname]) : $_REQUEST[$argname];
+  }
+ }
+$results = $base->$schema->__call ($function, $args);
+
+header ('Content-Type: application/json ; charset=utf-8');
+header ('Cache-Control: no-cache , private');
+header ('Pragma: no-cache');
+if ($results) 
+  echo json_encode ($results);
+else 
+  echo '[]';
+exit;
